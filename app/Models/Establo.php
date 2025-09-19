502a6b0c7a2ec95d50a3f07833c30bdc76d6e0a2
santhurt <santiruizhurt@gmail.com>
2025-09-19 04:16:31 -0500

    esquema establos

---
+++ app/Models/Establo.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Establo extends Model
+{
+    /** @use HasFactory<\Database\Factories\EstabloFactory> */
+    use HasFactory;
+}
