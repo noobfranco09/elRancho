b30a41404c19472f4e0144ff12bf285e8ff2c235
santhurt <santiruizhurt@gmail.com>
2025-09-19 04:19:14 -0500

    esquema veterinarios

---
+++ app/Models/Veterinario.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Veterinario extends Model
+{
+    /** @use HasFactory<\Database\Factories\VeterinarioFactory> */
+    use HasFactory;
+}
---
+++ database/migrations/2025_09_19_091703_create_veterinarios_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('veterinarios', function (Blueprint $table) {
+            $table->id();
+            $table->string('cedula', 45)->unique();
+            $table->string('nombre', 100);
+            $table->string('telefono', 45);
+            $table->string('correo', 100)->unique();
+            $table->text('especialidad')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('veterinarios');
+    }
+};
