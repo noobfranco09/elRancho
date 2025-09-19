04a6dcd8fec70dc1657e8515349074af3172247b
santhurt <santiruizhurt@gmail.com>
2025-09-19 04:09:49 -0500

    esquema empleados

---
+++ database/migrations/2025_09_19_090842_create_empleados_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('empleados', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre', 100)->nullable();
+            $table->string('cedula', 45)->unique();
+            $table->date('fecha_nacimiento')->nullable();
+            $table->string('telefono', 45);
+            $table->string('correo', 100)->unique();
+            $table->text('direccion')->nullable();
+            $table->enum('estado', ['activo', 'inactivo'])->default('inactivo');
+            $table->foreignId('rol_id')->nullable()->constrained('roles');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('empleados');
+    }
+};
