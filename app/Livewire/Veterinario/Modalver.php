4837a3bb6d7ac28089e722fe877520249215aebb
noobFranco09 <noobfranco09@gmail.com>
2025-10-15 13:10:59 -0500

    se agrega la funcionalidad al botón de ver más en el módulo de veterinarios

---
+++ app/Livewire/Veterinario/Modalver.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Livewire\Veterinario;
+
+use Livewire\Component;
+use App\Models\Veterinario;
+use App\Models\Revision;
+use Livewire\Features\SupportConsoleCommands\Commands\PublishCommand;
+use LivewireUI\Modal\ModalComponent;
+class Modalver extends ModalComponent
+{
+    public $veterinarioId;
+    public $revisiones;
+    public function render()
+    {
+        return view('livewire.veterinario.modalver');
+    }
+    public function mount(Veterinario $veterinario)
+    {
+        $this->revisiones = $veterinario->revisiones;
+    }
+    public static function modalMaxWidth(): string
+    {
+        return '2xl';
+    }
+}
--- resources/views/components/veterinarios/actions.blade.php
+++ resources/views/components/veterinarios/actions.blade.php
@@ -1,5 +1,5 @@
 <div class="flex items-center justify-center space-x-2">
-    <x-button variant="primary" @click="$dispatch('openModal', { component: 'veterinario.modalVer', arguments: { veterinario:{{ $veterinario->id }} } })" icon="visibility">
+    <x-button variant="primary" @click="$dispatch('openModal', { component: 'veterinario.modalver', arguments: { veterinario:{{ $veterinario->id }} } })" icon="visibility">
         Ver
     </x-button>
 
