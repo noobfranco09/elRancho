4e758be77d294b0c964f97a57f9327e91d13a120
santhurt <santiruizhurt@gmail.com>
2025-11-10 22:25:34 -0500

    add: ver animales de la venta

---
+++ app/Livewire/Venta/ModalAnimales.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Livewire\Venta;
+
+use App\Models\Animal;
+use App\Models\Venta;
+use LivewireUI\Modal\ModalComponent;
+
+class ModalAnimales extends ModalComponent
+{
+    public $animales;
+    public Venta $venta;
+
+    public function mount(Venta $venta)
+    {
+        $this->animales = Animal::where("venta_id", $venta->id)->get();
+    }
+
+    public function render()
+    {
+        return view('livewire.venta.modal-animales');
+    }
+}
--- resources/views/components/ventas/actions.blade.php
+++ resources/views/components/ventas/actions.blade.php
@@ -1,6 +1,6 @@
 <div class="flex items-center justify-center space-x-2">
 
-    <x-button variant="blue" @click="" icon="search">
+    <x-button variant="blue" @click="$dispatch('openModal', { component: 'venta.modal-animales', arguments: { venta:{{ $venta->id }} } })" icon="search">
         Animales
     </x-button>
 
