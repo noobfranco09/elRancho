698205154a8dd17ee607a3f42a107cb98354d671
grupo5 <grupo5@srv808810.hstgr.cloud>
2025-11-19 05:34:11 +0000

    backup pre configuracion reverb

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,7 +13,16 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+	$middleware->trustProxies(at: [
+            'proxy_network', 
+	],
+	headers: Request::HEADER_X_FORWARDED_FOR |
+                     Request::HEADER_X_FORWARDED_HOST |
+                     Request::HEADER_X_FORWARDED_PORT | // ¡CLAVE PARA EL PUERTO 8050!
+                     Request::HEADER_X_FORWARDED_PROTO |
+                     Request::HEADER_X_FORWARDED_AWS_ELB
+	);
+
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
